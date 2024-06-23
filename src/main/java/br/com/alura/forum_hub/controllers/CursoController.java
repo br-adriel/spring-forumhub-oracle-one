b655bed4b7e644de1fff9b8827ee661a7311197e
Adriel <adriel.fsantos@outlook.com>
2024-06-23 15:20:56 -0300

    feat: Add CursoController get mapping

---
+++ src/main/java/br/com/alura/forum_hub/controllers/CursoController.java
@@ -0,0 +1,25 @@
+package br.com.alura.forum_hub.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum_hub.domain.curso.CursoRepository;
+import br.com.alura.forum_hub.domain.curso.DadosListagemCurso;
+
+@RestController
+@RequestMapping("cursos")
+public class CursoController {
+	@Autowired
+	private CursoRepository repository;
+
+	@GetMapping
+	public ResponseEntity<Page<DadosListagemCurso>> listar(Pageable paginacao) {
+		var page = repository.findAll(paginacao).map(DadosListagemCurso::new);
+		return ResponseEntity.ok(page);
+	}
+}
