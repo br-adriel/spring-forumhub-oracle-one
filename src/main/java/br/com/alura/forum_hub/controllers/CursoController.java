c250e1c671a44fbbc5c7a3ab9b9ef69ea10a2d94
Adriel <adriel.fsantos@outlook.com>
2024-06-23 15:30:29 -0300

    feat: Add get mapping for a single 'curso'

--- src/main/java/br/com/alura/forum_hub/controllers/CursoController.java
+++ src/main/java/br/com/alura/forum_hub/controllers/CursoController.java
@@ -5,10 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum_hub.domain.curso.CursoRepository;
+import br.com.alura.forum_hub.domain.curso.DadosDetalhamentoCurso;
 import br.com.alura.forum_hub.domain.curso.DadosListagemCurso;
 
 @RestController
@@ -21,5 +23,11 @@ public class CursoController {
 	public ResponseEntity<Page<DadosListagemCurso>> listar(Pageable paginacao) {
 		var page = repository.findAll(paginacao).map(DadosListagemCurso::new);
 		return ResponseEntity.ok(page);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<DadosDetalhamentoCurso> detalhar(@PathVariable Long id) {
+		var curso = repository.getReferenceById(id);
+		return ResponseEntity.ok(new DadosDetalhamentoCurso(curso));
 	}
 }
