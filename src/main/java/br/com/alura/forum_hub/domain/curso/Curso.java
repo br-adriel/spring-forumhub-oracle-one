cde585830ee5e3bee0cb1b944f5b50e8b055f02d
Adriel <adriel.fsantos@outlook.com>
2024-06-23 15:20:00 -0300

    feat: Add Curso entity

---
+++ src/main/java/br/com/alura/forum_hub/domain/curso/Curso.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum_hub.domain.curso;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Curso")
+@Table(name = "tb_curso")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Curso {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+
+	@Enumerated(EnumType.STRING)
+	private CategoriaCurso categoria;
+}
