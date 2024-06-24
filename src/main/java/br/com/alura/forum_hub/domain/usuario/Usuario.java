9ca21d7d14baf4c7b2d8acd8900b1f294c6bc7dc
Adriel <adriel.fsantos@outlook.com>
2024-06-23 22:28:24 -0300

    feat: Add Usuario entity

---
+++ src/main/java/br/com/alura/forum_hub/domain/usuario/Usuario.java
@@ -0,0 +1,80 @@
+package br.com.alura.forum_hub.domain.usuario;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Usuario")
+@Table(name = "tb_usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario implements UserDetails {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+	private String email;
+	private String senha;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_perfil_usuario", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "perfil_id"))
+	private Set<Perfil> perfis;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return perfis.stream()
+				.map(p -> new SimpleGrantedAuthority(p.getNome()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
