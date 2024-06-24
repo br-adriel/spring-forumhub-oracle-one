4030822e0a4672a48c0a24fd911524af919f0642
Adriel <adriel.fsantos@outlook.com>
2024-06-23 22:29:28 -0300

    feat: Add SecurityFilter

---
+++ src/main/java/br/com/alura/forum_hub/infra/security/auth/SecurityFilter.java
@@ -0,0 +1,51 @@
+package br.com.alura.forum_hub.infra.security.auth;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.alura.forum_hub.domain.usuario.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+	@Autowired
+	private TokenService tokenService;
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	@Override
+	protected void doFilterInternal(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			FilterChain filterChain) throws ServletException, IOException {
+		var token = recuperarToken(request);
+
+		if (token != null) {
+			var subject = tokenService.getSubject(token);
+			var usuario = repository.findByEmail(subject);
+
+			var authentication = new UsernamePasswordAuthenticationToken(
+					usuario,
+					null,
+					usuario.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		var authorizationHeader = request.getHeader("Authorization");
+		if (authorizationHeader != null)
+			return authorizationHeader.replace("Bearer ", "");
+		return null;
+	}
+}
