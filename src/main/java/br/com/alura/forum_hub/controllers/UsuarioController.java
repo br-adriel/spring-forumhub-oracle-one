b3d5b5fe15d83b7107f094704690d953c7cbeb6b
Adriel <adriel.fsantos@outlook.com>
2024-06-24 00:13:24 -0300

    feat: Add Usuario details endpoint

--- src/main/java/br/com/alura/forum_hub/controllers/UsuarioController.java
+++ src/main/java/br/com/alura/forum_hub/controllers/UsuarioController.java
@@ -5,10 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum_hub.domain.usuario.UsuarioRepository;
+import br.com.alura.forum_hub.domain.usuario.dto.DadosDetalhamentoUsuario;
 import br.com.alura.forum_hub.domain.usuario.dto.DadosListagemUsuario;
 
 @RestController
@@ -21,5 +23,11 @@ public class UsuarioController {
 	public ResponseEntity<Page<DadosListagemUsuario>> listar(Pageable paginacao) {
 		var page = repository.findAll(paginacao).map(DadosListagemUsuario::new);
 		return ResponseEntity.ok().body(page);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<DadosDetalhamentoUsuario> detalhar(@PathVariable Long id) {
+		var usuario = repository.getReferenceById(id);
+		return ResponseEntity.ok(new DadosDetalhamentoUsuario(usuario));
 	}
 }
