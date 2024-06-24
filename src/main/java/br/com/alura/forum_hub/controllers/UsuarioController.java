9c763e023cc3832377b2e5df3d0aa18395679944
Adriel <adriel.fsantos@outlook.com>
2024-06-24 00:06:38 -0300

    feat: Add UsuarioController

---
+++ src/main/java/br/com/alura/forum_hub/controllers/UsuarioController.java
@@ -0,0 +1,25 @@
+package br.com.alura.forum_hub.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum_hub.domain.usuario.UsuarioRepository;
+import br.com.alura.forum_hub.domain.usuario.dto.DadosListagemUsuario;
+
+@RestController
+@RequestMapping("usuarios")
+public class UsuarioController {
+	@Autowired
+	private UsuarioRepository repository;
+
+	@GetMapping
+	public ResponseEntity<Page<DadosListagemUsuario>> listar(Pageable paginacao) {
+		var page = repository.findAll(paginacao).map(DadosListagemUsuario::new);
+		return ResponseEntity.ok().body(page);
+	}
+}
