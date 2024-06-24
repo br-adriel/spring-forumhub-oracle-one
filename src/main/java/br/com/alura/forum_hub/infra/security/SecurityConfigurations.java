f9ea3b9bc8f73c64425f8b2d47969e600850e804
Adriel <adriel.fsantos@outlook.com>
2024-06-23 22:30:03 -0300

    feat: Update SecurityConfigurations to handle authentication

--- src/main/java/br/com/alura/forum_hub/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum_hub/infra/security/SecurityConfigurations.java
@@ -1,16 +1,27 @@
 package br.com.alura.forum_hub.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.alura.forum_hub.infra.security.auth.SecurityFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+	@Autowired
+	private SecurityFilter securityFilter;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
@@ -19,6 +30,18 @@ public class SecurityConfigurations {
 				.authorizeHttpRequests((req) -> {
 					req.anyRequest().permitAll();
 				})
+				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(
+			AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
