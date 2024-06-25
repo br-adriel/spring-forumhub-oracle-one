cca0c1550740094b284ba8acd84b74d14b97edd6
Adriel <adriel.fsantos@outlook.com>
2024-06-25 12:01:04 -0300

    feat: Add TopicoController put endpoint

--- src/main/java/br/com/alura/forum_hub/controllers/TopicoController.java
+++ src/main/java/br/com/alura/forum_hub/controllers/TopicoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum_hub.domain.topico.TopicoService;
+import br.com.alura.forum_hub.domain.topico.dto.DadosAtualizacaoTopico;
 import br.com.alura.forum_hub.domain.topico.dto.DadosCadastroTopico;
 import br.com.alura.forum_hub.domain.topico.dto.DadosDetalhamentoTopico;
 import br.com.alura.forum_hub.domain.topico.dto.DadosListagemTopico;
@@ -57,5 +59,13 @@ public class TopicoController {
 	public ResponseEntity<Void> excluir(@PathVariable Long id) {
 		topicoService.excluir(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<DadosDetalhamentoTopico> atualizar(
+			@PathVariable Long id,
+			@RequestBody @Valid DadosAtualizacaoTopico dados) {
+		var topico = topicoService.atualizar(id, dados);
+		return ResponseEntity.ok().body(new DadosDetalhamentoTopico(topico));
 	}
 }
