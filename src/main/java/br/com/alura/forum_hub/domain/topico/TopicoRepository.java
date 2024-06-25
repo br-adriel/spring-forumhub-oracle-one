13dfdd457af124209b9fc83d8d18d38b22d0297d
Adriel <adriel.fsantos@outlook.com>
2024-06-25 00:49:36 -0300

    feat: Add TopicoRepository methods

--- src/main/java/br/com/alura/forum_hub/domain/topico/TopicoRepository.java
+++ src/main/java/br/com/alura/forum_hub/domain/topico/TopicoRepository.java
@@ -1,7 +1,26 @@
 package br.com.alura.forum_hub.domain.topico;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+	Page<Topico> findAllByOrderByDataCriacaoAsc(Pageable paginacao);
 
+	Page<Topico> findAllByCursoNomeAndDataCriacaoBetweenOrderByDataCriacaoAsc(
+			String nomeCurso,
+			LocalDateTime startDate,
+			LocalDateTime endDate,
+			Pageable paginacao);
+
+	Page<Topico> findAllByCursoNomeOrderByDataCriacaoAsc(
+			String nomeCurso,
+			Pageable paginacao);
+
+	Page<Topico> findAllByDataCriacaoBetweenOrderByDataCriacaoAsc(
+			LocalDateTime startDate,
+			LocalDateTime endDate,
+			Pageable paginacao);
 }
