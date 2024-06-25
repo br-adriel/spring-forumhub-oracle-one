0cf585d1c13e51dcc6a413ad896c70f05b5037da
Adriel <adriel.fsantos@outlook.com>
2024-06-25 12:00:02 -0300

    feat: Update validators to handle Topico update

--- src/main/java/br/com/alura/forum_hub/domain/topico/validation/CursoExiste.java
+++ src/main/java/br/com/alura/forum_hub/domain/topico/validation/CursoExiste.java
@@ -5,18 +5,29 @@ import org.springframework.stereotype.Component;
 
 import br.com.alura.forum_hub.domain.ValidacaoException;
 import br.com.alura.forum_hub.domain.curso.CursoRepository;
+import br.com.alura.forum_hub.domain.topico.dto.DadosAtualizacaoTopico;
 import br.com.alura.forum_hub.domain.topico.dto.DadosCadastroTopico;
 
 @Component
-public class CursoExiste implements ValidadorCadastroTopico {
+public class CursoExiste implements ValidadorCadastroTopico, ValidadorAtualizacaoTopico {
 	@Autowired
 	private CursoRepository cursoRepository;
 
 	@Override
 	public void validar(DadosCadastroTopico dados) {
-		var curso = cursoRepository.getReferenceById(dados.curso());
-		if (curso == null)
-			throw new ValidacaoException("Curso inválido");
+		validar(dados.curso());
 	}
 
+	@Override
+	public void validar(DadosAtualizacaoTopico dados) {
+		if (dados.curso() != null) {
+			validar(dados.curso());
+		}
+	}
+
+	private void validar(Long id) {
+		cursoRepository
+				.findById(id)
+				.orElseThrow(() -> new ValidacaoException("Curso inválido"));
+	}
 }
--- src/main/java/br/com/alura/forum_hub/domain/topico/validation/TituloDiferenteDeMensagem.java
+++ src/main/java/br/com/alura/forum_hub/domain/topico/validation/TituloDiferenteDeMensagem.java
@@ -3,13 +3,25 @@ package br.com.alura.forum_hub.domain.topico.validation;
 import org.springframework.stereotype.Component;
 
 import br.com.alura.forum_hub.domain.ValidacaoException;
+import br.com.alura.forum_hub.domain.topico.dto.DadosAtualizacaoTopico;
 import br.com.alura.forum_hub.domain.topico.dto.DadosCadastroTopico;
 
 @Component
-public class TituloDiferenteDeMensagem implements ValidadorCadastroTopico {
+public class TituloDiferenteDeMensagem implements ValidadorCadastroTopico, ValidadorAtualizacaoTopico {
 	@Override
 	public void validar(DadosCadastroTopico dados) {
-		if (dados.titulo().equalsIgnoreCase(dados.mensagem())) {
+		validar(dados.titulo(), dados.mensagem());
+	}
+
+	@Override
+	public void validar(DadosAtualizacaoTopico dados) {
+		if (dados.titulo() != null && dados.mensagem() != null) {
+			validar(dados.titulo(), dados.mensagem());
+		}
+	}
+
+	public static void validar(String titulo, String mensagem) {
+		if (titulo.equalsIgnoreCase(mensagem)) {
 			throw new ValidacaoException("Titulo e mensagem devem ser diferentes");
 		}
 	}
