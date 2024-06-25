f60dad9a4241204c9d75f9b9ae330d54d7844f07
Adriel <adriel.fsantos@outlook.com>
2024-06-25 00:50:21 -0300

    feat: Add TopicoService listar method

--- src/main/java/br/com/alura/forum_hub/domain/topico/TopicoService.java
+++ src/main/java/br/com/alura/forum_hub/domain/topico/TopicoService.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -43,5 +45,22 @@ public class TopicoService {
 				curso);
 		topicoRepository.save(topico);
 		return new DadosDetalhamentoTopico(topico);
+	}
+
+	public Page<Topico> listar(Pageable paginacao, String nomeCurso, Integer ano) {
+		if (nomeCurso != null && ano != null) {
+			LocalDateTime comecoAno = LocalDateTime.of(ano, 1, 1, 0, 0);
+			LocalDateTime fimAno = LocalDateTime.of(ano, 12, 31, 23, 59, 59);
+			return topicoRepository.findAllByCursoNomeAndDataCriacaoBetweenOrderByDataCriacaoAsc(nomeCurso, comecoAno,
+					fimAno, paginacao);
+		} else if (nomeCurso != null) {
+			return topicoRepository.findAllByCursoNomeOrderByDataCriacaoAsc(nomeCurso,
+					paginacao);
+		} else if (ano != null) {
+			LocalDateTime comecoAno = LocalDateTime.of(ano, 1, 1, 0, 0);
+			LocalDateTime fimAno = LocalDateTime.of(ano, 12, 31, 23, 59, 59);
+			return topicoRepository.findAllByDataCriacaoBetweenOrderByDataCriacaoAsc(comecoAno, fimAno, paginacao);
+		}
+		return topicoRepository.findAllByOrderByDataCriacaoAsc(paginacao);
 	}
 }
