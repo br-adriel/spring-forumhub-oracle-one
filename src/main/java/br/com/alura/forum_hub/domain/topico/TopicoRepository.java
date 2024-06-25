c4dc3d79588ae8b5d71057faf96ef3f9b2bd25f3
Adriel <adriel.fsantos@outlook.com>
2024-06-24 23:08:06 -0300

    feat: Add TopicoRepository

---
+++ src/main/java/br/com/alura/forum_hub/domain/topico/TopicoRepository.java
@@ -0,0 +1,7 @@
+package br.com.alura.forum_hub.domain.topico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+}
