100ad0c9d1e6b131b6c71f0f6090141c5bcd470f
Adriel <adriel.fsantos@outlook.com>
2024-06-25 22:58:27 -0300

    feat: Add RespostaController patch mapping

--- src/main/java/br/com/alura/forum_hub/controllers/RespostaController.java
+++ src/main/java/br/com/alura/forum_hub/controllers/RespostaController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -62,5 +63,11 @@ public class RespostaController {
 			@RequestBody @Valid DadosAtualizacaoResposta dados) {
 		var resposta = respostaService.atualizar(id, dados);
 		return ResponseEntity.ok(new DadosDetalhamentoResposta(resposta));
+	}
+
+	@PatchMapping("/{id}/solucao")
+	public ResponseEntity<Void> marcarComoSolucao(@PathVariable Long id) {
+		respostaService.marcarComoSolucao(id);
+		return ResponseEntity.ok().build();
 	}
 }
